6205ec836d56b9a31bf2b8c48f4d28af61248285
Akash <akashcj@gmail.com>
2018-06-15 11:23:37 +0530

    Components Primer and Initial Assignment

---
+++ template/src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-server',
+    templateUrl:'./server.component.html',
+    styles:[`
+    h3{
+        color: green;
+    }`]
+})
+export class ServerComponent{
+
+}
