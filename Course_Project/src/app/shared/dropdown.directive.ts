2ba758ddf701eadef72e9d00108c76c48a538022
Akash <akashcj@gmail.com>
2018-07-01 14:25:46 +0530

    Shorter code for making dropdowns to work, using host binding

--- Course_Project/src/app/shared/dropdown.directive.ts
+++ Course_Project/src/app/shared/dropdown.directive.ts
@@ -1,34 +1,16 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener } from '@angular/core';
+import { Directive, OnInit, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector:'[appDropdown]'
 })
 export class DropdownDirective implements OnInit{
+  @HostBinding('class.open') isOpen = false;
 
-  open:boolean = false;
+  constructor() {}
 
-  constructor(private elementRef:ElementRef, private renderer:Renderer2) {
-    console.log ("In constructor");
-   }
-
-  ngOnInit(){
-    console.log("In Init");
-  }
+  ngOnInit(){}
 
   @HostListener('click') clicked(eventData:Event){
-    console.log("In listener, value of open is:" + this.open);
-    if (!this.open)
-    {
-      console.log(this.renderer);
-      console.log(this.elementRef);
-      this.renderer.addClass(this.elementRef.nativeElement,"open");
-      this.open = true;
-    }
-    else
-    {
-      this.renderer.removeClass(this.elementRef.nativeElement,"open");
-      this.open = false;
-    }
-    console.log("open value is now:" + this.open);
+    this.isOpen = !this.isOpen;
   }
 }
