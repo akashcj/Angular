b8c5a16d00995a3243e1659fe472578e77755837
Akash C Joseph <Akash.Joseph@mea.ey.net>
2018-07-05 19:50:47 +0530

    Services Start

---
+++ services-start/src/app/account/account.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService, AccountsService]
+})
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+
+  constructor(private loggingService:LoggingService, private accountsService:AccountsService){}
+  onSetTo(status: string) {
+    this.accountsService.updateAccount(this.id, status);
+    this.loggingService.logStatus('A server status changed, new status: ' + status);
+  }
+}
---
+++ services-start/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from './accounts.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers:[AccountsService]
+})
+export class AppComponent implements OnInit{
+  accounts:{name:string, status:string}[] = [];
+
+  constructor(private accountsService:AccountsService){}
+
+  onAccountAdded(newAccount: {name: string, status: string}) {
+  }
+
+  ngOnInit(){
+    this.accounts = this.accountsService.accounts;
+  }
+
+  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+  }
+}
