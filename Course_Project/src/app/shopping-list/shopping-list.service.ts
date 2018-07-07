e229978e4902c4f2891274535a8cb837fdcec890
Akash <akashcj@gmail.com>
2018-07-07 07:53:32 +0530

    Added the ShoppingListService to the Course Project

--- Course_Project/src/app/shopping-list/shopping-list.service.ts
+++ Course_Project/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,22 @@
+import { Ingredient } from "../shared/ingredient.model";
+import { Output, EventEmitter } from "@angular/core";
+
 export class ShoppingListService{
+    @Output() ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredients:Ingredient[] = [
+      new Ingredient('Apples',5),
+      new Ingredient('Tomatoes', 10)
+    ];
+
+    getIngredients()
+    {
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredientToAdd:Ingredient)
+    {
+        this.ingredients.push(ingredientToAdd);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
     
 }
