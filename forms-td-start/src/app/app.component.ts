030ca46d4689ce5d8e7d663c8f27a0f0b917b0e0
Akash C Joseph <Akash.Joseph@xe04.ey.com>
2018-07-28 10:23:27 +0530

    form setValue and patchValue examples

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,15 +9,37 @@ import { NgForm } from '../../node_modules/@angular/forms';
 export class AppComponent {
 
   @ViewChild('f') inputForm: NgForm;
+  answer = '';
   defaultQuestion = 'pet';
+
+  genders: string[] = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.inputForm.form.patchValue({
+      userData: {
+        inputText: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: ElementRef) {
   //   console.log(form);
   // }
+
+  resetForm() {
+    this.inputForm.setValue(
+      {
+        userData: {
+        inputText: '',
+        inputEmail: '',
+        },
+        secret: 'pet',
+        userAnswer: '',
+        gender: 'male'
+      }
+    );
+  }
 
   onSubmit() {
     console.log(this.inputForm);
