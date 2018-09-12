b4eaac40f460d72c40d93d810395e7f1d1cdc195
Akash C Joseph <Akash.Joseph@xe04.ey.com>
2018-09-12 15:21:37 +0530

    updated for async pipe

--- pipes-start/src/app/app.component.ts
+++ pipes-start/src/app/app.component.ts
@@ -35,6 +35,10 @@ export class AppComponent {
 
   filterString: string;
 
+  delayedValue = new Promise((resolve, reject) => {
+    setTimeout(() => {resolve( 'stable' ); }, 2000);
+  });
+
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
