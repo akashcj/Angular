af7a7ce72dab78bc590409f43aac785257852c73
Akash <akashcj@gmail.com>
2019-02-21 22:44:19 +0530

    Http Client events

--- Course_Project/src/app/app.module.ts
+++ Course_Project/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { DataStorageService } from './shared/data-storage.service';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
@@ -22,7 +22,7 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     ShoppingListModule,
     AppRoutingModule,
-    HttpModule,
+    HttpClientModule,
     SharedModule,
     AuthModule,
     CoreModule
--- Course_Project/src/app/shared/data-storage.service.ts
+++ Course_Project/src/app/shared/data-storage.service.ts
@@ -1,30 +1,33 @@
-import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Injectable } from '@angular/core';
 
 import {Recipe} from '../recipes/recipe.model';
 import { AuthService } from '../auth/auth.service';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class DataStorageService {
 
     dbPath = 'https://ng-recipe-book-f9f03.firebaseio.com/recipe.json';
 
-    constructor(private httpService: Http, 
+    constructor(private httpclient: HttpClient, 
         private recipeService: RecipeService, 
         private authService: AuthService) {}
 
     saveRecipes() {
         let token = this.authService.getToken();
-        return this.httpService.put(this.dbPath + '?auth=' + token, this.recipeService.getRecipes());
+        return this.httpclient.put(this.dbPath + '?auth=' + token, this.recipeService.getRecipes(), {observe:'events'});
     }
 
     fetchRecipes() {
         let token = this.authService.getToken();
         // this.httpService.get(this.dbPath + '?auth=' + token).subscribe(
-        this.httpService.get(this.dbPath + '?auth=' + token).subscribe(
-            (data: Response) => {
-                const recipes: Recipe[] = data.json();
+        this.httpclient.get<Recipe[]>(this.dbPath + '?auth=' + token, {
+            observe:'body',
+            responseType: 'json'
+        }).subscribe(
+            (data) => {
+                const recipes: Recipe[] = data;
                 // if there is no ingredients array, adding an empty array
                 for (const recipe of recipes) {
                     if (!recipe.ingredients) {
